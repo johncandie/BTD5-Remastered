6090b9d298f1562cd17fd6ed827e1751ce3ef8b2
bahaaaldin214 <bahaaaldinmohammad@gmail.com>
2018-10-07 18:44:32 -0500

    dartmonkey placement finally near perfect

--- Scripts/Towers/DartMonkey.js
+++ Scripts/Towers/DartMonkey.js
@@ -36,21 +36,49 @@ function ClickedOnDartMonkey(){
       window.addEventListener ("click", function(e) {
         
         if(DartMonkeyIsClicked === true){
-          DartMonkeyPos = Math.round((e.pageY / SquareSize) * row + (e.pageX / SquareSize));
-          if(e.pageX < (row*SquareSize)-(row*6)-15 && e.pageY < (column*SquareSize)-(3*SquareSize) ){
-            console.log(DartMonkeyPos, MapSelected[DartMonkeyPos]);
+          DartMonkeyPos = Math.floor((e.pageY + SquareSize * 0.5) / SquareSize) * row + Math.floor((e.pageX + SquareSize * 0.5) / SquareSize);
+          if(e.pageX < (row*SquareSize)-(row*6)-15 && e.pageY < (column*SquareSize)-(3*SquareSize) && MapSelected[DartMonkeyPos] == 1){
             DartMonkeyIsClicked = false;
             DartMonkeys.push(new DartMonkey(e.pageX-25, e.pageY-25));
             Money -= DartMonkeyPrice;
+            console.log(DartMonkeyPos, MapSelected[DartMonkeyPos]);
           }
         }
       });
     }
   });
 }
+
+window.addEventListener("keydown", function(e){
+  if(e.keyCode == 81){
+    
+      DartMonkeyIsClicked = true;
+
+      window.addEventListener("mousemove", function(e){
+      hoverX = e.pageX;
+      hoverY = e.pageY;
+      //hoverX = Math.round((e.pageX / column) * SquareSize);
+      //hoverY = Math.round((e.pageY / row) * SquareSize);
+      //console.log((hoverY / SquareSize) * row + (hoverX / SquareSize))
+     });
+      window.addEventListener ("click", function(e) {
+        
+        if(DartMonkeyIsClicked === true){
+          DartMonkeyPos = Math.floor((e.pageY + SquareSize * 0.5) / SquareSize) * row + Math.floor((e.pageX + SquareSize * 0.5) / SquareSize);
+          if(e.pageX < (row*SquareSize)-(row*6)-15 && e.pageY < (column*SquareSize)-(3*SquareSize) && MapSelected[DartMonkeyPos] == 1){
+            DartMonkeyIsClicked = false;
+            DartMonkeys.push(new DartMonkey(e.pageX-25, e.pageY-25));
+            Money -= DartMonkeyPrice;
+            console.log(DartMonkeyPos, MapSelected[DartMonkeyPos]);
+          }
+        }
+      });
+    
+  }
+});
 function DartMonkeyDraw(){
   if(DartMonkeyIsClicked === true) {
-    context.drawImage(DartMonkeySrc, hoverX, hoverY, 50, 50);
+    context.drawImage(DartMonkeySrc, hoverX - 25, hoverY - 25, 50, 50);
   }
   for(let i = 0; i < DartMonkeys.length; i++){
     DartMonkeys[i].draw();
