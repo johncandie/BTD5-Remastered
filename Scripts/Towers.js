5de7cda5a5e41a0b346a03ea4b09eec698cd6831
Bahaa <bahaaaldinmohammad@gmail.com>
2019-01-01 17:15:22 -0600

    Update Towers.js

--- Scripts/Towers.js
+++ Scripts/Towers.js
@@ -13,8 +13,8 @@ var TowerSelected;
 var clickX;
 var clickY;
 
-var hoverX;
-var hoverY;
+var hoverX = 0;
+var hoverY = 0;
 
 var PlaceTaken = false;
 
@@ -22,6 +22,14 @@ var PlaceTaken = false;
 window.addEventListener("click", function(e) {
     clickX = e.pageX;
     clickY = e.pageY;
+    if(!TowerIsClicked) {
+      CheckSpot(clickX, clickY);
+      if(PlaceTaken){
+        var tower = CheckSpot(clickX, clickY);
+        console.log(tower);
+      }
+    }
+
     if (clickX >= 1200 && clickX <= 1250 && clickY >= 100 && clickY <= 150 && Money >= DartMonkeyPrice) ClickedOnDartMonkey();
 });
 window.addEventListener("keydown", function(e) {
@@ -43,6 +51,9 @@ function CheckSpot(x, y) {
             break;
         } else PlaceTaken = false;
     }
+    if(PlaceTaken){
+      return Towers[parseInt(i)];
+    }
 }
 
 
@@ -52,36 +63,21 @@ function DisplayTowers() {
     for (let T = 0; T < Towers.length; T++) {
         Towers[T].draw();
         for (let B = 0; B < Bloons.length; B++) {
-            var dx = Towers[T].x - Bloons[B].x;
-            var dy = Towers[T].y - Bloons[B].y;
-            var Distance = Math.sqrt(dx * dx + dy * dy);
+            let dx = Towers[T].x - Bloons[B].x;
+            let dy = Towers[T].y - Bloons[B].y;
+            let Distance = Math.sqrt(dx * dx + dy * dy);
 
             if (Distance <= Towers[T].range + 50) {
                 Towers[T].shoot(Bloons[B].x, Bloons[B].y);
             }
         }
     }
-
-    for (let D = 0; D < Darts.length; D++) {
-        Darts[D].animate();
-        for (let B = 0; B < Bloons.length; B++){
-
-            context.font = "50px Comic Sans MS";
-            if(Darts[D].Dx + 10 >= Bloons[B].x && Darts[D].Dx <= Bloons[B].x + 50 && Darts[D].Dy + 10>= Bloons[B].y && Darts[D].Dy <= Bloons[B].y + 50){
-              Bloons[B].level--;
-              Money++;
-              Darts[D].pierce--;
-              if(Darts[D].pierce >= 0) Darts.splice(D, 1);
-              if(Darts[D].range >= Darts[D].sx + Darts[D].range) Darts.splice(D, 1);
-            }
-        }
-    }
     var shadeBlack = true;
     if(PlaceTaken === true) {shadeBlack = false;} else {shadeBlake = true}
-    if(MapSelected[Math.floor(hoverY/50)][Math.floor(hoverX/50)] != 1){shadeBlack = false;}
     var Color = 'rgb(255, 0, 0, 0.52)';
     if (TowerIsClicked) {
         context.drawImage(TowerSelected, hoverX - 25, hoverY - 25, 50, 50);
+        if(MapSelected[Math.floor(hoverY/50)][Math.floor(hoverX/50)] != 1){shadeBlack = false;}
         Color = 'rgb(0, 0, 0, 0.5)';
         switch (shadeBlack) {
             case false:
@@ -94,16 +90,34 @@ function DisplayTowers() {
         context.fillStyle = Color;
         context.fill();
     }
+
+    for (let D = 0; D < Darts.length; D++) {
+        Darts[D].animate();
+        for (let B = 0; B < Bloons.length; B++){
+
+            context.font = "50px Comic Sans MS";
+            if(Darts[D].Dx + 10 >= Bloons[B].x && Darts[D].Dx <= Bloons[B].x + 50 && Darts[D].Dy + 10>= Bloons[B].y && Darts[D].Dy <= Bloons[B].y + 50){
+                Bloons[B].level--;
+                Money++;
+                Darts[D].pierce--;
+                if(Darts[D].pierce <= 0) {Darts.splice(D, 1); break}
+                  let dx = Darts[D].sx - Darts[D].Dx;
+                  let dy = Darts[D].sy - Darts[D].Dy;
+                  let Distance = Math.sqrt(dx * dx + dy * dy);
+                if(Distance >= Darts[D].range) {Darts.splice(D, 1); break}
+            }
+        }
+    }
 }
 
-const Dart = function(Sx, Sy, Bx, By, pierce, range) {
+const Dart = function(Sx, Sy, Bx, By, pierce, range, layers) {
         this.Dx = Sx;
         this.Dy = Sy; //Dx/Dy is where to draww
     this.animate = function() {
-        this.pierce = pierce;this.range = range;
+        this.pierce = pierce;this.range = range; this.layers = layers;
         this.sx = Sx; this.sy = Sy;
         var Nx = Bx - Sx; //Sx/Sy is where it starts, so from the dart monkey
-        var Ny = By - Sy; //Nx/Ny is the new x/y, so the slope, here is where it misses because its not in simplest form
+        var Ny = By - Sy; //Nx/Ny is the new x/y, so t,he slope, here is where it misses because its not in simplest form
         var oneX = 0;
         var oneY = 0;
         
